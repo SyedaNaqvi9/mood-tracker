fd129b678fad6ba2dd3298a965e50ef2e159dfb8
SyedaNaqvi9 <70096819+SyedaNaqvi9@users.noreply.github.com>
2025-03-08 15:53:04 +1100

    Updated

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ let list = document.querySelector("#moodHistory");
 let clearBtn = document.querySelector("#clearMood");
 let body = document.querySelector("body");
 let quote = document.querySelector("#quote");
+let playlistContainer = document.querySelector("#playlist");
+let footer = document.querySelector("footer");
 
-const today = new Date().toLocaleDateString();
+const today = new Date().toLocaleString();
 dateDisplay.textContent = `📅 Today's Date: ${today}`;
 
 let moodHistory = JSON.parse(localStorage.getItem("moods")) || [];
@@ -23,10 +25,24 @@ let angryQuote = [`\"When anger rises, think of the consequences!"`, `\"The best
 let tiredQuote = [`\"Breathe. It's just a bad day, not a bad life!"`, `\"It's okay to be a glowstick; sometimes we need to break before we shine!"`,  `\"Fatigue is the price we pay for the work we have put in!"`];
 let excitedQuote = [`\"Life is too short to not be excited about something!"`, `\"The best way to predict your future is to create it.!"`,  `\"Dance like nobody's watching, sing like no one's listening!"`];
 
+const playlists = {
+    "Happy": "https://open.spotify.com/embed/playlist/37i9dQZF1DXdPec7aLTmlC",
+    "Sad": "https://open.spotify.com/embed/playlist/37i9dQZF1DX3rxVfibe1L0",
+    "Angry": "https://open.spotify.com/embed/playlist/37i9dQZF1DWY6tYEFs22tT",
+    "Tired": "https://open.spotify.com/embed/playlist/37i9dQZF1DX3Ogo9pFvBkY",
+    "Excited": "https://open.spotify.com/embed/playlist/37i9dQZF1DX8FwnYE6PRvL"
+};
 
 let index;
 
-
+btn.addEventListener("click", () => {
+    let mood = moodValue.value;
+    if (playlists[mood]) {
+        playlistContainer.innerHTML = `<iframe src="${playlists[mood]}" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+    } else {
+        playlistContainer.innerHTML = `<p>No playlist available for this mood.</p>`;
+    }
+});
 
 moodValue.addEventListener("change", () => {
         btn.disabled = false;
@@ -66,6 +82,7 @@ btn.addEventListener("click", () => {
         body.style.backgroundImage = "url('https://syedanaqvi9.github.io/mood-tracker/images/excited.png')";
         index = Math.floor(Math.random() * excitedQuote.length);
         quote.textContent = excitedQuote[index];
+        
     } 
 
     let currentMood = `${today} - ${moodValue.value}`;
@@ -91,6 +108,9 @@ clearBtn.addEventListener("click", () => {
     body.style.backgroundImage = "url('https://syedanaqvi9.github.io/mood-tracker/images/mood-finder.png')";
     quote.textContent = "Today's Quote";
     moodValue.value = "";
+    playlistContainer.innerHTML = "";
+
+
     }
 
 })
